8baee3d4d533035e8933a8d11b6f38b255ee1d52
Flavien JARRY <flavien.jarry@sfr.com>
2020-05-04 16:15:31 +0200

    chat solo working

--- websocket-server.js
+++ websocket-server.js
@@ -1,1 +1,22 @@
 const WebSocket = require('ws');
+/**
+ * CLIENT
+ */
+const webSocketServer = new WebSocket.Server({ port: process.env.WS_PORT });
+
+webSocketServer.on('connection', webSocket => {
+    console.log('Connection established');
+
+    webSocket.on('message', messageEvent => {
+        console.log('A message was sent')
+        webSocket.send(messageEvent);
+    });
+});
+
+webSocketServer.on('error', (error) => {
+    console.log('Error occured ', error);
+});
+
+webSocketServer.on('close', function close() {
+    console.log('Connection closed');
+});
